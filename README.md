392c43d4d75f05b10358d6a08dcac844d314ffec
Jacob Albano <jacob.albano@gmail.com>
2024-01-21 17:06:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# CmoaUserscriptTemplate
+
+Run ```npm install```
+
+## Things to change:
+
+### package.json:
+- name: the file name (version will be appended)
+- description: the userscript description
+- config.port: the listen port for dev mode
+
+### meta.json
+- name: the name of the userscript
+- match: urls to match
+
+### readme.txt
+- get rid of all this
---
+++ rollup.config.mjs
@@ -0,0 +1,60 @@
+import commonjs from '@rollup/plugin-commonjs';
+import fs from 'fs';
+import metablock from 'rollup-plugin-userscript-metablock';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import pkg from './package.json' assert { type: 'json' }
+import meta from './meta.json' assert { type: 'json' }
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import { string } from 'rollup-plugin-string';
+import { argv } from 'process';
+
+fs.mkdir('dist/', { recursive: true }, () => null);
+
+function plugins() {
+    return [
+        nodeResolve(),
+        commonjs(),
+        string({
+            include: [
+                'template/**',
+            ],
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            ENVIRONMENT: JSON.stringify('production'),
+            preventAssignment: true,
+        }),
+    ];
+}
+
+function* terserIfEnabled() {
+    console.log(JSON.stringify(argv));
+    if (argv.includes('--config-minify'))
+        yield terser();
+}
+    
+export default [{
+    input: 'src/userscript.js',
+    output: {
+        file: `dist/${pkg.name}${pkg.version}.user.js`,
+        format: 'iife',
+        name: 'userscript',
+        banner: () => `\n/*\n${ fs.readFileSync('./LICENSE', 'utf8') }*/`,
+        sourcemap: true,
+    },
+    plugins: [
+        ...plugins(),
+        ...terserIfEnabled(),
+        metablock({
+            file: './meta.json',
+            override: {
+                name: meta.name,
+                version: pkg.version,
+                description: pkg.description,
+                homepage: pkg.homepage,
+                author: pkg.author,
+            },
+        }),
+    ],
+}];
